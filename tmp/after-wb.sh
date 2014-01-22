6afa75fa32454c302267a33a6539615d52331e76
Yatin Khadilkar <ykhadilkar@reisys.com>
2014-01-21 19:25:43 -0500

    Create after-wb.sh
    adding after web script from geodatagov repo

---
+++ tmp/after-wb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+chkconfig --level 2345 httpd on
+chkconfig --level 2345 memcached on
+setsebool -P httpd_can_network_connect 1
+setsebool -P httpd_tmp_exec on
+initctl reload-configuration
+rm -f /etc/httpd/conf.d/welcome.conf
+
+if [ ! -f /usr/lib64/libxmlsec1-openssl.so ];
+then
+     ln -s /usr/lib64/libxmlsec1-openssl.so.1.2.16 /usr/lib64/libxmlsec1-openssl.so
+fi
+
+service memcached start
+service httpd restart
+
+chown apache:apache -R /usr/lib/ckan/src/ckanext-geodatagov/ckanext/geodatagov/dynamic_menu
